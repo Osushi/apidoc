6bf453b3d053bad07eeb95fe8698088ddd2c725e
Osushi <tdu.future.i@gmail.com>
2018-11-29 00:34:12 +0900

    Create apidoc class test

---
+++ tests/Unit/ApidocTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Osushi\Apidoc\Apidoc;
+
+class ApidocTest extends TestCase
+{
+    /**
+     * @test
+     * @expectedException \Exception
+     */
+    public function testGetInstance()
+    {
+        $apiDoc = Apidoc::getInstance();
+    }
+    
+    public function testInit()
+    {
+        $apiDoc = Apidoc::init();
+        $this->assertTrue($apiDoc instanceof Apidoc);
+    }
+    
+    public function testGetInstanceAfterInit()
+    {
+        $apiDoc = Apidoc::getInstance();
+        $this->assertTrue($apiDoc instanceof Apidoc);
+    }
+    
+    public function testCall()
+    {
+        $_SERVER['argv'] = [];
+        $apiDoc = Apidoc::getInstance();
+        try {
+            $apiDoc->render();
+            $this->assertTrue(true);
+        } catch (\Exeption $e) {
+            $this->fail('Unable to call method');
+        }
+    }
+}
