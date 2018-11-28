45301c847af02e64069212afb7d6b2e00565dae4
Osushi <tdu.future.i@gmail.com>
2018-11-29 00:34:26 +0900

    Create config class test

---
+++ tests/Unit/ConfigTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Osushi\Apidoc\Config;
+
+class ConfigTest extends TestCase
+{
+    private $confg;
+
+    public function setUp()
+    {
+        $this->config = new Config();
+    }
+    
+    public function testSetAndGetConfig()
+    {
+        $this->config->setConfig($this->config::OUTPUT_PATH, 'something');
+        $actual = $this->config->getConfig($this->config::OUTPUT_PATH);
+        $this->assertEquals('something', $actual);
+        
+        $this->assertFalse($this->config->getConfig('invalid'));
+    }
+}
