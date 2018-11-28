aa238251b18cdec2742d9f2e9d6261dbcf101f0c
Osushi <tdu.future.i@gmail.com>
2018-11-29 00:35:00 +0900

    Create response class test

---
+++ tests/Unit/ResponseTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Osushi\Apidoc\Response;
+
+class ResponseTest extends TestCase
+{
+    private $response;
+
+    public function setUp()
+    {
+        $this->response = new Response();
+    }
+
+    public function testConstruct()
+    {
+        $response = new Response([
+            'code' => 200,
+            'headers' => [
+                'Content-Type' => 'application/json; charset=utf-8',
+            ],
+            'body' => '{"created_at": "2015-04-21T14:55:09.351Z"}',
+        ]);
+
+        $this->assertEquals(200, $response->getCode());
+        $this->assertEquals([
+            'Content-Type' => 'application/json; charset=utf-8',
+        ], $response->getHeaders());
+        $this->assertEquals([
+            'created_at' => '2015-04-21T14:55:09.351Z',
+        ], $response->getBody());
+    }
+
+    public function testSetAndGetCode()
+    {
+        $this->response->setCode(404);
+        $this->assertEquals(404, $this->response->getCode());
+    }
+    
+    public function testSetAndGetHeaders()
+    {
+        $this->response->setHeaders([
+            'Content-Type' => 'application/json; charset=utf-8',
+        ]);
+        $this->assertEquals([
+            'Content-Type' => 'application/json; charset=utf-8',
+        ], $this->response->getHeaders());
+    }
+    
+    public function testSetAndGetBody()
+    {
+        $this->response->setBody('{"created_at": "2015-04-21T14:55:09.351Z"}');
+        $this->assertEquals([
+            'created_at' => '2015-04-21T14:55:09.351Z',
+        ], $this->response->getBody());
+    }
+}
